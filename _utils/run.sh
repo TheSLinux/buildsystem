1d8cb9448518e8fc2952d92d9b8f4580393ed4d7
Ky-Anh Huynh <kyanh@theslinux.org>
2013-04-22 20:15:24 +0700

    + link to official repository

--- _utils/run.sh
+++ _utils/run.sh
@@ -4,6 +4,7 @@
 # Author : Anh K. Huynh
 # License: GPL v2 (http://www.gnu.org/licenses/gpl-2.0.html)
 # Date   : 2013 March 30th
+# Home   : https://github.com/TheSLinux/buildsystem/tree/_utils
 
 _msg() {
   echo ":: $@"
