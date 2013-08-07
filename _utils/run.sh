f918f49305886bf59148d8f68a9f1b818622ccb5
Ky-Anh Huynh <kyanh@theslinux.org>
2013-08-07 20:30:48 +0700

    + copyright information

--- _utils/run.sh
+++ _utils/run.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
-
+#
 # Purpose: Various tools (See function docs.)
 # Author : Anh K. Huynh
 # License: GPL v2 (http://www.gnu.org/licenses/gpl-2.0.html)
 # Date   : 2013 March 30th
 # Home   : https://github.com/TheSLinux/buildsystem/tree/_utils
+#
+# Copyright (c) 2013 Anh K. Huynh <kyanh@theslinux.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 _msg() {
   echo ":: $@"
